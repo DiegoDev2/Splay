60b5a21041f47fb4bf17849b9f5848d397ee407a
Mac <mac@Macs-MacBook-Pro.local>
2024-12-20 01:35:21 -0400

    fix scroll

--- components/ui/scroll.tsx
+++ components/ui/scroll.tsx
@@ -1,31 +1,34 @@
-"use client";
+"use client"; // Asegura que este archivo se ejecute en el cliente
+
 import React, { useEffect } from "react";
 import locomotiveScroll from "locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 
 interface ScrollEffectProps {
-  children: React.ReactNode;  
+  children: React.ReactNode;
 }
 
 const ScrollEffect: React.FC<ScrollEffectProps> = ({ children }) => {
   useEffect(() => {
-    const container = document.querySelector(".scroll-container") as HTMLElement;
+    if (typeof window !== "undefined" && typeof document !== "undefined") {
+      const container = document.querySelector(".scroll-container") as HTMLElement;
 
-    if (container) {
-      const scroll = new locomotiveScroll({
-        el: container,
-        smooth: true,
-        multiplier: 0.4, 
-        class: "is-inview",
-      });
+      if (container) {
+        const scroll = new locomotiveScroll({
+          el: container,
+          smooth: true,
+          multiplier: 0.6,
+          class: "is-inview",
+        });
 
-      return () => {
-        scroll.destroy();
-      };
+        return () => {
+          scroll.destroy();
+        };
+      }
     }
   }, []);
 
-  return <div className="scroll-container">{children}</div>; 
+  return <div className="scroll-container">{children}</div>;
 };
 
 export default ScrollEffect;
